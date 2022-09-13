f14317bb96f896ebe6d9e0c27d295a965dfdb6d5
“sensurusirupus” <sensurusirupus@gmail.com>
2022-09-13 22:33:40 +0000

    ‘world3’

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
 // import components
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Brands from './components/Brands';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Testimonials from './components/Testimonials';
-import BackTopBtn from './components/BackTopBtn';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Brands from "./components/Brands";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
+import BackTopBtn from "./components/BackTopBtn";
 
 const App = () => {
   return (
-    <div className='bg-white relative'>
+    <div className="bg-white relative">
       <Header />
       <Hero />
-      <Brands />
+      <Testimonials />
       <About />
       <Skills />
       <Portfolio />
--- src/components/TestiSlider.js
+++ src/components/TestiSlider.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
 // testimonials data
-import { testimonials } from '../data';
+import { testimonials } from "../data";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import '../swiper.css';
+import "swiper/css";
+import "swiper/css/pagination";
+import "../swiper.css";
 
 // import required modules
-import { Autoplay, Pagination } from 'swiper';
-
+import { Autoplay, Pagination } from "swiper";
 
 const TestiSlider = () => {
-
   return (
     <>
       <Swiper
@@ -28,28 +26,22 @@ const TestiSlider = () => {
           disableOnInteraction: false,
         }}
         modules={[Autoplay, Pagination]}
-        className='mySwiper'
+        className="mySwiper"
       >
         {testimonials.map((item, index) => {
           const { authorImg, authorText, authorName, authorPosition } = item;
           return (
             <SwiperSlide key={index}>
-              <div
-                className='flex flex-col lg:flex-row gap-12 lg:gap-32'
-              >
-                <div
-                  className='w-48 h-48 lg:w-[328px] lg:h-[328px]'
-                >
-                  <img className='rounded-2xl' src={authorImg} alt='' />
+              <div className="flex flex-col lg:flex-row gap-12 lg:gap-32">
+                <div className="w-60 h-60 lg:w-[828px] lg:h-[828px]">
+                  <img className="rounded-2xl  " src={authorImg} alt="" />
                 </div>
-                <div
-                  className='flex flex-col max-w-3xl'
-                >
-                  <h5 className='font-body text-2xl mb-8 italic font-normal'>
+                <div className="flex flex-col max-w-3xl">
+                  <h5 className="font-body text-2xl mb-8 italic font-normal">
                     {authorText}
                   </h5>
                   <div>
-                    <p className='text-lg text-accent'>{authorName}</p>
+                    <p className="text-lg text-accent">{authorName}</p>
                     <p>{authorPosition}</p>
                   </div>
                 </div>
